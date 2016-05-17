bf79da56e4edb05a20606295f83f2c0e851b5938
text <270081484@qq.com>
2016-05-18 00:22:40 +0800

    5.18凌晨作品

--- src/cn/list/control/EventManage.java
+++ src/cn/list/control/EventManage.java
@@ -1,13 +1,32 @@
 package cn.list.control;
 
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+
 import cn.list.model.Event;
 import cn.list.workinterface.IEvent;
 
 public class EventManage implements IEvent {
 
 	@Override
-	public void CreateEvent() {
+    public void CreateEvent(int ID) {
 		// 创建新事件
+		 Scanner a=new Scanner(System.in); 
+		 int i=0;
+		 Event event=new Event();
+		 
+		 System.out.print("事件名称：");
+		 String name = a.nextLine();
+		 event.setName(name);//事件输入
+		 System.out.print("起始时间（日期-小时:分钟）：");
+		 SimpleDateFormat df = new SimpleDateFormat("dd-HH:mm"); 
+		 String begintimetext=a.nextLine();
+		 Date begintime;
+		 
+//		 i=a.nextInt();
+		 System.out.println("事件名称："+event.getName());
 	}
 
 	@Override
@@ -33,5 +52,9 @@ public class EventManage implements IEvent {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	public static void main(String[] args){
+		//测试
+		EventManage a=new EventManage();
+		a.CreateEvent(1);
+	}
 }
--- src/cn/list/workinterface/IEvent.java
+++ src/cn/list/workinterface/IEvent.java
@@ -3,7 +3,7 @@ package cn.list.workinterface;
 import cn.list.model.Event;
 
 public interface IEvent {
-	public void CreateEvent();
+	public void CreateEvent(int ID);
 	public void AddEvent(int ID);
 	public void DelEvent(int ID);
 	public void ModifyEvent(int ID);
